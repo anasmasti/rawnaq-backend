129b71560abcbad69bfa7ec5e20b7f4f6bdd4be7
anasmasti <anasmasti@hotmail.com>
2022-08-08 11:49:58 +0100

    Add MongoDb connection and Article Schema

---
+++ db/config.db.ts
@@ -0,0 +1,9 @@
+const mongoDbConnection = main()
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/rawnaq");
+}
+
+module.exports = mongoDbConnection;
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const http = require("http");
 const express = require("express");
-
+const mongoDbConnection = require("./db/config.db.ts");
 const app = express();
 const server = http.createServer(app);
+
+mongoDbConnection
 
 app.route('/', (req, res) => {
     res.send('done')
